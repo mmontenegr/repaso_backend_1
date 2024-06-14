3400d4262d3bc3dc2e1515b809744d1e40ed7c8c
Mariela Montenegro <33108324+mmontenegr@users.noreply.github.com>
2024-06-14 15:58:17 -0300

    Add files via upload
    merge manual de develop

---
+++ apps.js
@@ -0,0 +1,11 @@
+const express = require ("express");
+const app = express();
+
+app.get('/', (req, res)  => {
+  res.send("Cac Deploy");
+});
+
+const port = process.env.port || 3000;
+
+app.listen (port, () => console.log ("http://localhost:" + port));
+
